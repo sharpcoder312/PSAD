b3892c00e14019356f6a362ccfeee82ef1079a55
Bebop <tjr50999@naver.com>
2021-09-19 18:14:59 +0900

    Add: [백준 1단계 10951번 추가]

---
+++ BaekJoon/10951/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+const testCaseArray = [];
+let i = 0;
+while (i < input.length) {
+  const tempValue = input[i].split(' ').map((item) => +item);
+  testCaseArray.push(tempValue[0]+tempValue[1]);
+  console.log(testCaseArray[i]);
+  i++;
+}
+
